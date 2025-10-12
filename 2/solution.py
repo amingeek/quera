8bfc99ed3c173761edf531580d796cb1ba9a4611
Amin <seyyed.amin.8809@gmail.com>
2025-10-12 23:57:28 +0330

    fix q 2

--- 2/solution.py
+++ 2/solution.py
@@ -1,23 +1,28 @@
+import re
+
 def process(path):
     text = read_page(path)
-    text = spl(text)
-    res = search(text, "<a")
+    text = clean_html(text)
+    res = search(text)
     return res
 
 
-def search(text, key):
-    n = 0
-    for i in text:
-        if len(i) == len(key):
-            if i == key:
-                n += 1
-    return n
+def clean_html(text):
+    text = re.sub(r'<!--.*?-->', '', text, flags=re.DOTALL)
+    text = re.sub(r'<script.*?>.*?</script>', '', text, flags=re.DOTALL | re.IGNORECASE)
+    text = re.sub(r'<style.*?>.*?</style>', '', text, flags=re.DOTALL | re.IGNORECASE)
+    return text
+
+
+def search(text):
+    matches = re.findall(r'<\s*a\b[^>]*>', text, flags=re.IGNORECASE)
+    return len(matches)
+
 
 def read_page(name):
-    with open(name) as f:
+    with open(name, encoding="utf-8") as f:
         return f.read()
+
 
 def spl(text):
     return text.split()
-
-
