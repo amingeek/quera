880fa7a1c650d46b42d76283d1e2705d59b5ad71
Amin <seyyed.amin.8809@gmail.com>
2025-11-11 23:17:14 +0330

    add q 3

---
+++ 3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func GuessMyNumber(game Game) string {
+	low := 1
+	high := 100
+	var guess int
+	var res string
+
+	for low <= high {
+		guess = (low + high) / 2
+		res = game.CheckNumber(guess)
+
+		if res == "Correct" {
+			return "Your Number was " + itoa(guess)
+		} else if res == "My Number is Greater" {
+			low = guess + 1
+		} else if res == "My Number is Lower" {
+			high = guess - 1
+		} else {
+			return "Your Number was " + itoa(guess)
+		}
+	}
+
+	return "Number not found in range"
+}
+
+func itoa(num int) string {
+	return fmt.Sprintf("%d", num)
+}
